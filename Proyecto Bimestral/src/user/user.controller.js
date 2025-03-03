ba8e201da947ec9474aa1b1338706337a416bf6c
dchupina-2023120 <dchupina-2023120@kinal.edu.gt>
2025-03-03 15:51:18 -0600

    Proyecto casi casi terminado

--- Proyecto Bimestral/src/user/user.controller.js
+++ Proyecto Bimestral/src/user/user.controller.js
@@ -71,16 +71,7 @@ export const getUsers = async(req,res)=>{
 }
 
 
-//Obtener un usuario por ID
-export const getUserById = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id).select("-password");
-        if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ message: "Error al recuperar el usuario ", error });
-    }
-}
+
 
 
 // Actualizar  usuario
